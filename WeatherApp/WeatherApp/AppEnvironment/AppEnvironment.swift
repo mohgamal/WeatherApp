deb8e11c2c8780087667f124128253e025203235
Mohammed Abdelaty <dev.mohgamal@gmail.com>
2021-06-21 08:32:47 +0200

    change main app folder structure and create app environment

---
+++ WeatherApp/WeatherApp/AppEnvironment/AppEnvironment.swift
@@ -0,0 +1,14 @@
+//
+//  AppEnvironment.swift
+//  WeatherApp
+//
+//  Created by Mohammed Abd El-Aty on 21/06/2021.
+//
+
+import Foundation
+
+class AppEnvironment {
+    let baseURL = "https://www.metaweather.com/api/"
+    
+    let getCityByWoeidURL = "location/"
+}
--- WeatherAppPresentation/WeatherAppPresentation/Scenes/CityWeatherScene/CityWeatherView.swift
+++ WeatherAppPresentation/WeatherAppPresentation/Scenes/CityWeatherScene/CityWeatherView.swift
@@ -6,13 +6,35 @@
 //
 
 import UIKit
+import Combine
 
 class CityWeatherView: UIViewController {
+    
+    public let cityWeatherVM: CityWeatherVM
+    private var cancellable: AnyCancellable?
+    
+    private var cancellables: Set<AnyCancellable> = []
+    
+    var appDI: AppDIInterface
+    var coordinator: Coordinator
+    public var spinner = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
+    
+    public init(appDI: AppDIInterface, cityWeatherVM: CityWeatherVM, coordinator: Coordinator) {
+        self.appDI = appDI
+        self.cityWeatherVM = cityWeatherVM
+        self.coordinator = coordinator
+        super.init(nibName: "CityWeatherView", bundle: Bundle(for: CityWeatherView.self))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         updateUI()
+        bindViewModel()
     }
     
     
@@ -20,6 +42,40 @@ class CityWeatherView: UIViewController {
         let gradientLayer = Utils().generateGradientLayer()
         gradientLayer.frame = self.view.bounds
         self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
+    
+    private func bindViewModel() {
+        cityWeatherVM.objectWillChange.sink { [weak self] in
+            guard let self = self else {
+                return
+            }
+            
+            self.spinner.stopAnimating()
+            
+            if self.cityWeatherVM.error != nil {
+                self.viewAlert(with: self.cityWeatherVM.error)
+            } else {
+               
+            }
+            
+        }.store(in: &cancellables)
+    }
+    
+    private func viewAlert(with error: Error?) {
+        let alert = UIAlertController(title: "Error Occured", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    public func addSpinner() {
+        spinner.color = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.startAnimating()
+        view.addSubview(spinner)
+        
+        spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
     
 }
